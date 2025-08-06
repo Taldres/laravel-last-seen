ae45319f4f7931e12fb6a7f01d33aae311efb581
Dennis Petersmann <git@taldres.dev>
2025-08-06 17:57:14 +0200

    chore(tests): update phpstan level to 9 and enhance PHPUnit environment variables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,22 +3,10 @@
 namespace Taldres\LastSeen\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Taldres\LastSeen\LastSeenServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
-protected $loadEnvironmentVariables = true;
-    protected function getEnvironmentSetUp($app): void
-    {
-        $packageRoot = __DIR__.'/../';
-
-        if (file_exists($packageRoot)) {
-            $dotenv = \Dotenv\Dotenv::createImmutable($packageRoot, '.env.testing');
-            $dotenv->safeLoad();
-        }
-    }
-
     protected function getPackageProviders($app): array
     {
         return [
